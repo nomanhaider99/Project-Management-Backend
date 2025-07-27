a91734b7585ae566bb83e8112a550357e4d5195c
Noman Haider <noman567n@gmail.com>
2025-07-27 15:07:13 +0500

    updated envs

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   app.use(cookieParser());
   app.enableCors({
     origin: 'http://localhost:4200',
     credentials: true
   });
-  await app.listen(8000);
+
+  const port = configService.get<number>('PORT') || 8000;
+
+  await app.listen(port);
 }
 bootstrap();
